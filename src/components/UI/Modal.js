654dbd102c1cddc4391f9f26c41fcfb752687b30
ROHAN4535 <grohan468@gmail.com>
2023-10-21 16:16:03 +0530

    Worked on cart open and close

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,10 +1,12 @@
-import { Fragment } from "react";
+import { Fragment } from 'react';
 import ReactDOM from 'react-dom';
-import classes from "./Modal.module.css";
+
+import classes from './Modal.module.css';
 
 const Backdrop = (props) => {
-  return <div className={classes.backdrop} />;
+  return <div className={classes.backdrop} onClick={props.onClose}/>;
 };
+
 const ModalOverlay = (props) => {
   return (
     <div className={classes.modal}>
@@ -12,12 +14,13 @@ const ModalOverlay = (props) => {
     </div>
   );
 };
-const portalElement = document.getElementById("overlays");
+
+const portalElement = document.getElementById('overlays');
 
 const Modal = (props) => {
   return (
     <Fragment>
-      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
       {ReactDOM.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
