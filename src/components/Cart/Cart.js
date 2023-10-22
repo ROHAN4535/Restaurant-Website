5cfb4419690e6b3c9721d33dae7c16a929165c38
ROHAN4535 <grohan468@gmail.com>
2023-10-22 16:40:32 +0530

    Implemented the cart items page

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,24 +1,35 @@
+import { useContext } from "react";
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
+import CartContext from "../../store/cart-context";
 
 const Cart = (props) => {
+  const cartCntx = useContext(CartContext);
+  let total = 0;
+
   const cartItems = (
-    <ul className={classes['cart-items']}>
-      {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
-        <li>{item.name}</li>
+    <ul className={classes["cart-items"]}>
+      {cartCntx.items.map((item) => (
+        <li>
+          Name: {item.name} - Price: {item.price} - Quantity:{item.quantity}
+        </li>
       ))}
     </ul>
   );
+
+  cartCntx.items.forEach((item) => {
+    total = total + Number(item.price) * item.quantity;
+  });
 
   return (
     <Modal onClose={props.onClose}>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>${total}</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button--alt']} onClick={props.onClose}>
+        <button className={classes["button--alt"]} onClick={props.onHideCart}>
           Close
         </button>
         <button className={classes.button}>Order</button>
--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,18 +1,27 @@
+import React, { useContext } from "react";
 import classes from './MealItemForm.module.css';
 import Input from '../../UI/Input';
+import CartContext from "../../../store/cart-context";
 
 const MealItemForm = (props) => {
+  const cartcntx = useContext(CartContext);
+  const addItemToCart = (event) => {
+    event.preventDefault();
+    const Quantity = document.getElementById("amount_" + props.id).value;
+    cartcntx.addItem({ ...props.item, quantity: Quantity });
+  };
+
   return (
     <form className={classes.form}>
       <Input label="Amount" input={{
-        id: 'amount',
+        id: "amount_" + props.id,
         type: 'number',
         min: '1',
         max: '5',
         step: '1',
         defaultValue: '1'
       }} />
-      <button>+ Add</button>     
+      <button onClick={addItemToCart}>+ Add</button>    
     </form>
   );    
 };
